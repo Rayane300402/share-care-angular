6df2c7619163107aadf62f4d9fafe116df5f7fc7
Rayane300402 <raycaratboulsi@gmail.com>
2025-05-13 14:54:16 +0300

    added route +m navbar + footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { BlogPageComponent } from './pages/blog-page/blog-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+const routes: Routes = [
+    { path: '',      component: HomePageComponent },
+  { path: 'blog',  component: BlogPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'about', component: AboutPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
